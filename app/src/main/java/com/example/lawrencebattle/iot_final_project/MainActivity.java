55558fbb492c365405ef3a84c8a97e1c764b07b9
Battle <lbattl13@uncc.edu>
2018-10-29 10:55:31 -0400

    Updated 29Oct10

--- app/src/main/java/com/example/lawrencebattle/iot_final_project/MainActivity.java
+++ app/src/main/java/com/example/lawrencebattle/iot_final_project/MainActivity.java
@@ -2,12 +2,33 @@ package com.example.lawrencebattle.iot_final_project;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public TextView SenseView;
+    public EditText UserNameIP,PasswordIP;
+    public Button LoginButton,ResetButton,Register,LogoutButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //Declare Buttons
+        LoginButton=(Button) findViewById(R.id.LoginButton);
+        ResetButton=(Button) findViewById(R.id.ResetButton);
+        Register= (Button) findViewById(R.id.Register);
+        LogoutButton=(Button) findViewById(R.id.LogoutButton);
+
+        //Declare EditText
+        UserNameIP = (EditText) findViewById(R.id.UserNameIP);
+        PasswordIP = (EditText) findViewById(R.id.PasswordIP);
+
+
     }
 }
