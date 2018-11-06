8aa23e086d02431c5575f6afd5fad541c212774d
leedagr8 <ljohn214@uncc.edu>
2018-11-06 15:15:10 -0500

    added java methods for registering user.

---
+++ app/src/main/java/com/example/lawrencebattle/iot_final_project/Constants.java
@@ -0,0 +1,10 @@
+package com.example.lawrencebattle.iot_final_project;
+
+public class Constants {
+
+    //URL of local Host
+    private static final String ROOT_URL = "http://127.0.0.1/";
+
+    //Register URL
+    public static String URL_REGISTER = ROOT_URL +"registerUser.php";
+}
