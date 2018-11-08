92e6c2a67d4f9f9b0192bdaaa560094089224bea
Battle <lbattl13@uncc.edu>
2018-11-08 11:55:31 -0500

    8Nov18Battle

---
+++ app/src/main/java/com/example/lawrencebattle/iot_final_project/ViewGenerators.java
@@ -0,0 +1,53 @@
+package com.example.lawrencebattle.iot_final_project;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.Button;
+
+public class ViewGenerators extends AppCompatActivity {
+
+    public Button BackButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //Button
+        BackButton = (Button) findViewById(R.id.BackButton);
+
+
+        //Sends User to a specific website
+        setContentView(R.layout.activity_view__generators);
+
+        String url = "https://services.swpc.noaa.gov/images/geospace-1-day.png";
+
+         WebView gens=(WebView) findViewById(R.id.generator_site);
+
+         gens.loadUrl(url);
+
+
+    }
+
+  //Not sure if there should be an @Override here
+    //@Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+
+
+            //Action Listeners
+
+            //Sends PowerUtility user to MainActivity
+            case R.id.BackButton:
+                Intent intentback = new Intent(this, power_utility.class);
+                startActivity(intentback);
+                break;
+
+
+
+        }
+
+    }
+}
--- app/src/main/java/com/example/lawrencebattle/iot_final_project/power_utility.java
+++ app/src/main/java/com/example/lawrencebattle/iot_final_project/power_utility.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.content.Intent;
 
 
 public class power_utility extends AppCompatActivity implements View.OnClickListener {
@@ -23,13 +24,45 @@ public class power_utility extends AppCompatActivity implements View.OnClickList
         market = (Button) findViewById(R.id.market);
         logout_button = (Button) findViewById(R.id.logout_button);
 
-        //Action Listeners
+
 
 
     }
 
-    @Override
+   // @Override
     public void onClick(View v) {
+        switch (v.getId()) {
+
+
+            //Action Listeners
+
+            //Sends PowerUtility user to View_Generators
+            case R.id.view_generator:
+                Intent intentviewgens= new Intent(this, ViewGenerators.class);
+                startActivity(intentviewgens);
+                break;
+
+            //Sends PowerUtility user to Manage_Generators
+            case R.id.manage_generator:
+            Intent intentmanagegens = new Intent(this, ManageGenerators.class);
+            startActivity(intentmanagegens);
+            break;
+
+            //Sends PowerUtility user to Market
+            case R.id.market:
+                Intent intentmarket = new Intent(this, Market.class);
+                startActivity(intentmarket);
+                break;
+
+            //Sends PowerUtility user to MainActivity
+            case R.id.logout_button:
+                Intent intentlogout = new Intent(this, MainActivity.class);
+                startActivity(intentlogout);
+                break;
+
+
+
+        }
 
     }
 }
