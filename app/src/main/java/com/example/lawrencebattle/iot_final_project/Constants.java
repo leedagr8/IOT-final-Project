90c64338e663e8c380ad510bed647fdff7ac69a5
Battle <lbattl13@uncc.edu>
2018-11-19 14:30:44 -0500

    update

--- app/src/main/java/com/example/lawrencebattle/iot_final_project/Constants.java
+++ app/src/main/java/com/example/lawrencebattle/iot_final_project/Constants.java
@@ -3,7 +3,7 @@ package com.example.lawrencebattle.iot_final_project;
 public class Constants {
 
     //URL of local Host
-    private static final String ROOT_URL = "http://127.0.0.1/";
+    private static final String ROOT_URL = "http://192.168.64.2/";
 
     //Register URLi
     public static String URL_REGISTER = ROOT_URL +"registerUser.php";
